96b3ba27b037d6d9df86dca50fcedf28f318bb20
GwendolineNoirtin <gwendoline.noirtin@devinci.fr>
2017-04-05 09:53:40 +0200

    Add files via upload

---
+++ TP1 - IF/script.js
@@ -0,0 +1,6 @@
+function homePageLoading (url, id){
+    <code id="id" class="json kljs"></code>
+      url = 'url';
+      elementID = 'id';
+      fonctionRequeteApi(http://bitcoin.mubiz.com/info, bitcoin/index.html);
+}
